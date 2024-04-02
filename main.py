90dde7e7c83c9fbfa7526a7836640d4aa083265a
EkSVaY <159280377+EkSVaY@users.noreply.github.com>
2024-04-02 22:51:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ water_freeman = 15_000
 belief_freeman = 0
 rand_events = [1, 2, 3, 4]
 random.shuffle(rand_events)
+kvartal = 0
 
 
 if story.story_line():
@@ -27,20 +28,20 @@ if story.story_line():
     PART_1. The history of Arakis
     '''
     if story.a_part_1(rep_atreides, army_atreides, tech_atreides, spice_atreides):
-        rep_atreides += 10
+        rep_atreides += 200
     else:
         army_atreides -= 4
-        rep_atreides -= 10
+        rep_atreides -= 50
         worms_freeman -= 1
 
     input("\nPress Enter\n")
 
     if story.h_part_1(rep_harcoon, army_harcoon, tech_harcoon, spice_harcoon):
         army_freeman -= 5
-        rep_harcoon += 10
+        rep_harcoon += 50
     else:
         army_harcoon -= 4
-        rep_harcoon -= 10
+        rep_harcoon -= 50
 
     input("\nPress Enter\n")
 
@@ -63,11 +64,55 @@ if story.story_line():
 
     input("\nPress Enter\n")
 
+    
+    '''
+    RANDOM EVENTS
+    '''
     for i_events in rand_events:
-        events.atreides(i_events)
-        events.harcoon(i_events)
-        events.empire(i_events)
-        events.freeman(i_events)
+
+        input("\nPress Enter\n")
+        atr_kvest = events.atreides(i_events)
+        input("\nPress Enter\n")
+        har_kvest = events.harcoon(i_events)
+        input("\nPress Enter\n")
+        emp_kvest = events.empire(i_events)
+        input("\nPress Enter\n")
+        free_kvest = events.freeman(i_events)
+
+        if i_events == 1:
+            if atr_kvest:
+                army_atreides -= 9
+                tech_atreides += 1
+            else:
+                army_atreides -= 1010
+                rep_harcoon += 100
+        if i_events == 2:
+            if atr_kvest:
+                spice_atreides += 5
+                rep_atreides += 100
+            else:
+                water_freeman += 100
+                rep_atreides -= 10
+        if i_events == 3:
+            if atr_kvest == False:
+                army_atreides -= 1000
+                spice_atreides -= 2
+            else:
+                rep_atreides += 10
+
+        spice_atreides += 5
+        spice_harcoon += 5
+        kvartal += 1
+        print(f"Итоги за {kvartal} квартал года")
+        print(f"\nДом Атридесов: Репутация {rep_atreides}, Армия {army_atreides},"
+              f" Техника {tech_atreides}, Специи {spice_atreides}\n")
+        print(f"Дом Харконненов: Репутация {rep_harcoon}, Армия {army_harcoon},"
+              f" Техника {tech_harcoon}, Специи {spice_harcoon}\n")
+        print(f"Империя: Деньги {money_empire}, флот {fleet_empire}, заговоры {conspiracies_empire},"
+              f" доверие великих домов {approval_empire}\n")
+        print(f"Фримены: Люди {army_freeman}, Шаи-Хулуды {worms_freeman}, Вода {water_freeman},"
+              f" Вера {belief_freeman}\n")
+        input("\nPress Enter\n")
 
 
 
